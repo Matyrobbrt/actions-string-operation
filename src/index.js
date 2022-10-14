326f1018f6ca239733801a9d717d70cbbb613652
Matyrobbrt <matyrobbrt@gmail.com>
2022-10-14 22:07:13 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import * as core from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+run();
+
+async function run() {
+    const mode = core.getInput("mode", { required: true })
+    const input = core.getInput("input", { required: true })
+
+    let result;
+    if (mode == "isBlank") {
+        result = isBlank(input)
+    } else if (mode == "parseTeam") {
+        result = parseTeam(input)
+    }
+
+    core.setOutput("result", result);
+}
+
+async function isBlank(input) {
+    return input.trim() == "" ? "true" : "false";
+}
+async function parseTeam(input) {
+    return input.substring(input.indexOf("/") + 1, input.length)
+}
